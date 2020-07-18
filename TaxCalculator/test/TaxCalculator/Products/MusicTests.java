18d0c12a822eaefcd3743071db913bbb6e9ba499
Victor Betts <v_betts22@hotmail.com>
2020-07-18 18:13:13 -0500

    Created getDescription() test with no parameter in Music to test default description and forced failure

--- TaxCalculator/test/TaxCalculator/Products/MusicTests.java
+++ TaxCalculator/test/TaxCalculator/Products/MusicTests.java
@@ -12,4 +12,12 @@ public class MusicTests {
         String expected = "Bad Hair Day";
         Assert.assertEquals(actual, expected);
     }
+
+    @Test
+    public void testGetDescription_GetDefaultDescription() {
+        Product music = Music.getInstance(11.49);
+        String actual = music.getDescription();
+        String expected = "Bad Hair Day";
+        Assert.assertEquals(actual, expected);
+    }
 }
