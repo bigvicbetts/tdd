af2fdc3cd277b2915792085b4deba0b0a4b38c99
Victor Betts <v_betts22@hotmail.com>
2020-07-18 18:22:55 -0500

    Added Fragrance test class, created getDescription() test and forced failure

---
+++ TaxCalculator/test/TaxCalculator/Products/FragranceTests.java
@@ -0,0 +1,15 @@
+package TaxCalculator.Products;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FragranceTests {
+
+    @Test
+    public void testGetDescription() {
+        Product fragrance = Fragrance.getInstance(11.49, "Something Smells");
+        String actual = fragrance.getDescription();
+        String expected = "Something stinks";
+        Assert.assertEquals(actual, expected);
+    }
+}
