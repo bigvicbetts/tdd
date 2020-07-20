d29bfd93f9b747b80cda6e0887183f00cc0943e8
Victor Betts <v_betts22@hotmail.com>
2020-07-19 19:04:59 -0500

    Changed the new test for calculateTotalPrice that requires tax to be rounded to display the actual expected value. Test passed

--- TaxCalculator/test/TaxCalculator/TaxCalculatorTests.java
+++ TaxCalculator/test/TaxCalculator/TaxCalculatorTests.java
@@ -49,7 +49,7 @@ public class TaxCalculatorTests {
     public void testCalculateTotalPrice_RoundingNecessary() {
         TaxCalculator taxCalculator = TaxCalculator.getInstance();
         BigDecimal actual = taxCalculator.calculateTotalPrice(Food.getInstance(11.25), true);
-        BigDecimal expected = new BigDecimal(String.valueOf("11.81"));
+        BigDecimal expected = new BigDecimal(String.valueOf("11.85"));
         Assert.assertEquals(actual, expected);
     }
 }
